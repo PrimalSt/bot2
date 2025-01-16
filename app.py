df42ed16c01e08bf2ad754217e40586e9f05f89b
PrimalSt <103900366+PrimalSt@users.noreply.github.com>
2025-01-16 17:23:59 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import os
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.webhook.aiohttp_server import setup_application
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
 
 # Получение токена из переменной окружения
-TOKEN = os.getenv('BOT_TOKEN', '7717648561:AAELGQaKsOQNaNDu3u7gOUTX9AvlBYoqPVc')
+TOKEN = os.getenv('BOT_TOKEN')
+if not TOKEN:
+    raise ValueError("Токен бота не найден. Убедитесь, что переменная BOT_TOKEN установлена.")
 
 # Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
@@ -47,14 +49,15 @@ async def help_handler(message: Message):
         "- /help для получения справки")
 
 async def on_startup(app: web.Application):
-    await bot.set_webhook("https://https://bot2-ksjg.onrender.com/webhook")
+    webhook_url = "https://https://bot2-ksjg.onrender.com/webhook"  # Замените на ваш домен
+    await bot.set_webhook(webhook_url)
 
 async def on_shutdown(app: web.Application):
     await bot.delete_webhook()
 
 # Создание веб-приложения для обработки вебхуков
 app = web.Application()
-setup_application(app, dp, bot=bot)
+SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path="/webhook")
 app.on_startup.append(on_startup)
 app.on_shutdown.append(on_shutdown)
 
